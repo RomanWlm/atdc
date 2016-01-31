06bd31530f904a60a7a36e8bc4b4e511c54d6d12
RomanWlm <roman.wilhelm@scaledrisk.com>
2016-01-31 23:45:51 +0100

    Product and Category Model

--- atdc_site/catalog/models.py
+++ atdc_site/catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    label = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('date published')
+
+class Product(models.Model):
+    label = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('date published')
+    category =models.ForeignKey(
+        Category,
+        models.SET_NULL,
+        blank=True,
+        null=True, )
+    price = models.DecimalField(..., max_digits=5, decimal_places=2)
+    details = models.CharField(max_length=1500)
