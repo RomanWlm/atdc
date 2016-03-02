926bbce64a167d6df15d3cc442e7cdccaa3e5e16
RomanWlm <roman.wilhelm@scaledrisk.com>
2016-03-02 23:12:00 +0100

    Playing with views

--- atdc_site/catalog/urls.py
+++ atdc_site/catalog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<product_name>[a-z]+)/search/$', views.search, name='search'),
 ]
--- atdc_site/catalog/views.py
+++ atdc_site/catalog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    response = "<html><body>"
+    response += "<h2> Catalogue </h2>"
+    response += "<br>Current time is %s" % datetime.datetime.now().time()
+    response += "</html></body>"
+    return HttpResponse(response)
+
+def search(request, product_name):
+    response = "<html><body>"
+    response += "<h2> Search </h2>"
+    response += "<br>Search details for product named "+product_name+" ..."
+    response += "<br>Current time is %s" % datetime.datetime.now().time()
+    response += "</html></body>"
+    return HttpResponse(response)
